90d9560e4a820d42c687d0f4ae66992a9f13a500
Oushan Skeen <inta.soul@gmail.com>
2020-02-14 23:29:00 +0300

    hope all okey

--- tests/3_service/controllers/hello.unit.test.js
+++ tests/3_service/controllers/hello.unit.test.js
@@ -2,16 +2,43 @@ const sayHello = require('../../../lib/4_core/interactors/sayHello');
 const helloController = require('../../../lib/3_service/controllers/hello');
 jest.mock('../../../lib/4_core/interactors/sayHello');
 
+
+//const sayHelloWorld = async(req,res) =>{
+//    return await res.json({message:sayHello()})
+//};
+
+let mockRequest = () => {
+    const req = {}
+    //req.body = jest.fn().mockReturnValue(req)
+    //req.params = jest.fn().mockReturnValue(req)
+    return req
+  }
+let mockResponse = () => {
+    const res = {}
+    //res.send = jest.fn().mockReturnValue(res)
+    //res.status = jest.fn().mockReturnValue(res)
+    res.json = jest.fn().mockReturnValue(res)
+    return res
+};
+
 describe('#sayHelloWorld', () => {
-    test('should resolves', async () => {
-    // given
-    sayHello.mockImplementationOnce(() => 'Bonjour monde !');
-    // when
-    const response = await helloController.sayHelloWorld();
-    //const response = helloController.sayHelloWorld();
-    // then
-    //expect(response).toBe({message:{'Bonjour monde !'});
-    expect(response).toEqual({message:'Bonjour monde !'});
+    test('should resolves', async (done) => {
+    //given
+        // do mock request
+        const req = mockRequest();
+        // do mock response
+        const res = mockResponse();
+        //fo mock domain function
+        sayHello.mockImplementationOnce(() => 'Bonjour monde !');
+    
+    //when
+    await helloController.sayHelloWorld(req,res);
+
+    //then
+    expect(res.json)
+        .toHaveBeenCalledWith({message:'Bonjour monde !'});
+
+    done();
   });
 });
 /*
